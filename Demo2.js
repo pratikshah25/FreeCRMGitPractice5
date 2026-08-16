7375900ad6bac4ddd099f3af4b3d607d0d364ef5
unknown <itspratikshah25.1990@gmail.com>
2026-08-16 09:18:41 +0530

    Updated Demo2 file code

--- Demo2.js
+++ Demo2.js
@@ -2,3 +2,11 @@ console.log("Demo2 creaded by Tester 2");
 console.log("Demo2 creaded by Tester 2");
 console.log("Demo2 creaded by Tester 2");
 console.log("Demo2 creaded by Tester 2");
+
+console.log("Tester1 have updated existing test cases code1");
+console.log("Tester1 have updated existing test cases code1");
+console.log("Tester1 have updated existing test cases code1");
+console.log("Tester1 have updated existing test cases code1");
+console.log("Tester1 have updated existing test cases code1");
+console.log("Tester1 have updated existing test cases code1");
+console.log("Tester1 have updated existing test cases code1");
